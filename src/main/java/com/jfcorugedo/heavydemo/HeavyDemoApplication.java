512affc55a84467d96e67625a290fc9456c3d881
Juan Fernandez-Corugedo Igual <juan.fernandez-corugedo.igual@ing.com>
2019-12-20 16:06:35 +0100

    First version

---
+++ src/main/java/com/jfcorugedo/heavydemo/HeavyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jfcorugedo.heavydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeavyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeavyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfcorugedo/heavydemo/HeavyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfcorugedo.heavydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeavyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
